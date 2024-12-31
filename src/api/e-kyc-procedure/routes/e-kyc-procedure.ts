8a6fc9677b57ee47c017eda67cf62acd14de85ae
bhanukiran-gida <bhanukiran.t@gida.io>
2024-12-31 10:32:21 +0530

    adding ekyc-procedure schema (#4)

---
+++ src/api/e-kyc-procedure/routes/e-kyc-procedure.ts
@@ -0,0 +1,7 @@
+/**
+ * e-kyc-procedure router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::e-kyc-procedure.e-kyc-procedure');
---
+++ src/api/e-kyc-procedure/services/e-kyc-procedure.ts
@@ -0,0 +1,7 @@
+/**
+ * e-kyc-procedure service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::e-kyc-procedure.e-kyc-procedure');
