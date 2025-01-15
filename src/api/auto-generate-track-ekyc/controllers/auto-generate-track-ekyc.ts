0e9be4da09c9a8af60e861c34880fa5a97b9f064
adnankhan700 <adnan.khan@gida.io>
2025-01-15 15:57:31 +0530

    Feat/auto generate and track (#24)
    * start with quickActions
    
    * merge with develop
    
    * pull
    
    * resolve merge
    
    * completed quickActions
    
    * fix quickAction and start with dashboard
    
    * add schema for quick-actions and dashboard base-1
    
    * policy-manual-lonking finished
    
    * completed document-upload
    
    * change name to health-document-uploader
    
    * add documentUploadModal
    
    * pull and merge with dev
    
    * fix health-track schema
    
    * complete schema for health-claims
    
    * fix motor-claim-filing
    
    * complete schema for motor-policy-transfer
    
    * add schema for auto-generate-track-ekyc
    
    * fix schema for generate

---
+++ src/api/auto-generate-track-ekyc/controllers/auto-generate-track-ekyc.ts
@@ -0,0 +1,7 @@
+/**
+ * auto-generate-track-ekyc controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::auto-generate-track-ekyc.auto-generate-track-ekyc');
---
+++ src/api/auto-generate-track-ekyc/services/auto-generate-track-ekyc.ts
@@ -0,0 +1,7 @@
+/**
+ * auto-generate-track-ekyc service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::auto-generate-track-ekyc.auto-generate-track-ekyc');
