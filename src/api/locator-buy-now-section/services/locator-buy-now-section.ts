aa68fc76ec8a1d5b2070befaf6a984b929061089
ZainAhmed17 <62997346+ZainAhmed17@users.noreply.github.com>
2025-01-28 11:19:26 +0530

    added locators (#38)
    Co-authored-by: R Sai Pratheep Reddy <saipratheepreddy2818@gmail.com>

---
+++ src/api/locator-buy-now-section/services/locator-buy-now-section.ts
@@ -0,0 +1,7 @@
+/**
+ * locator-buy-now-section service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::locator-buy-now-section.locator-buy-now-section');
---
+++ src/api/locator/routes/locator.ts
@@ -0,0 +1,7 @@
+/**
+ * locator router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::locator.locator');
