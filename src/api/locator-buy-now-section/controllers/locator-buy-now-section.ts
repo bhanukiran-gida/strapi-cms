a81f63c168098ac22275f43d74f92536f050b58f
ZainAhmed17 <62997346+ZainAhmed17@users.noreply.github.com>
2025-01-28 13:07:52 +0530

    Locators (#41)
    * added locators
    
    * locator type
    
    * added buy now section
    
    ---------
    
    Co-authored-by: R Sai Pratheep Reddy <saipratheepreddy2818@gmail.com>

--- src/api/locator-buy-now-section/controllers/locator-buy-now-section.ts
+++
@@ -1,7 +0,0 @@
-/**
- * locator-buy-now-section controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::locator-buy-now-section.locator-buy-now-section');
