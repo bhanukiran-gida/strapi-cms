c7282fd3451fb7385fb49d3f80eb9168e44bb7fa
chethzzzz <163298937+chethzzzz@users.noreply.github.com>
2025-01-30 15:28:43 +0530

    inital commit (#47)
    * inital commit
    
    * env
    
    ---------
    
    Co-authored-by: Neel <neel.gandhi@gida.io>

---
+++ src/api/travel-policy-detail/controllers/travel-policy-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * travel-policy-detail controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::travel-policy-detail.travel-policy-detail');
