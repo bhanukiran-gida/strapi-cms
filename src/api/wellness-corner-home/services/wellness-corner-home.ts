ae6fbc9cfeb4e49bcba4f18b8f101e997c5b5974
ZainAhmed17 <62997346+ZainAhmed17@users.noreply.github.com>
2025-01-31 19:40:55 +0530

    Fix/about us struct (#57)
    * about us struct changes
    
    * struct changes
    
    * cahnges
    
    * award struct change
    
    * changes
    
    * added change
    
    * structure changes
    
    * changes
    
    * changes
    
    * structure added
    
    ---------
    
    Co-authored-by: Zain Ahmed <zain.ahmed@gida.io>

---
+++ src/api/wellness-corner-home/services/wellness-corner-home.ts
@@ -0,0 +1,7 @@
+/**
+ * wellness-corner-home service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::wellness-corner-home.wellness-corner-home');
