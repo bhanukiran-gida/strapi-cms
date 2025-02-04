da908288b97a2c1eb473bbee3c69a042d7373fb9
bhanukiran-gida <bhanukiran.t@gida.io>
2025-02-04 15:19:15 +0530

    Feat/landing components (#67)
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * change
    
    * changes
    
    * update: ekyc procedure schema
    
    * changes
    
    * adding blogs structure
    
    * changes
    
    * changes
    
    * changes
    
    * changes

---
+++ src/api/blogs-landing-page/controllers/blogs-landing-page.ts
@@ -0,0 +1,7 @@
+/**
+ * blogs-landing-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::blogs-landing-page.blogs-landing-page');
