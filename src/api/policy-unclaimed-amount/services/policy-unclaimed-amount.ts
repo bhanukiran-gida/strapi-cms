198afeac93e10eeb301c421241e91453d7afa01e
chethzzzz <163298937+chethzzzz@users.noreply.github.com>
2025-02-06 11:38:32 +0530

    added fields (#78)
    * added fields
    
    * changes

---
+++ src/api/policy-unclaimed-amount/services/policy-unclaimed-amount.ts
@@ -0,0 +1,7 @@
+/**
+ * policy-unclaimed-amount service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::policy-unclaimed-amount.policy-unclaimed-amount');
