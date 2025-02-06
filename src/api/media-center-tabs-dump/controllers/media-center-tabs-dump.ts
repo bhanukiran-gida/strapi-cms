45c8a070f53ba8a368888925f1394f6a7f8e2749
Neel <163390201+neel-gida@users.noreply.github.com>
2025-02-06 17:52:50 +0530

    Cms/media center tabs (#81)
    * structure made
    
    * updated

---
+++ src/api/media-center-tabs-dump/controllers/media-center-tabs-dump.ts
@@ -0,0 +1,7 @@
+/**
+ * media-center-tabs-dump controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::media-center-tabs-dump.media-center-tabs-dump');
---
+++ src/api/media-center-tabs-dump/routes/media-center-tabs-dump.ts
@@ -0,0 +1,7 @@
+/**
+ * media-center-tabs-dump router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::media-center-tabs-dump.media-center-tabs-dump');
