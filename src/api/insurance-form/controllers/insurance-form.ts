995c24437d076c336b8608b9e278dfa2cc74afc2
ZainAhmed17 <62997346+ZainAhmed17@users.noreply.github.com>
2025-02-07 17:33:11 +0530

    Calculators/cms (#87)
    * about us struct changes
    
    * added plp and calculators cms
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Zain Ahmed <zain.ahmed@gida.io>
    Co-authored-by: R Sai Pratheep Reddy <saipratheepreddy2818@gmail.com>

---
+++ src/api/insurance-form/controllers/insurance-form.ts
@@ -0,0 +1,7 @@
+/**
+ * insurance-form controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::insurance-form.insurance-form');
---
+++ src/api/insurance-form/services/insurance-form.ts
@@ -0,0 +1,7 @@
+/**
+ * insurance-form service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::insurance-form.insurance-form');
