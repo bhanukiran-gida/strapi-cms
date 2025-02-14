57e4eff658f373316808f7bcc62ef178a122892e
Neel <163390201+neel-gida@users.noreply.github.com>
2025-02-14 13:57:30 +0530

    Calc/insurance pages (#137)
    * new components made
    
    * car insurance added for calc
    
    * insurance added in calc
    
    * bike insurance comps updated
    
    * bike insurance structured
    
    * health added

---
+++ src/api/calculators-bike-insurance/controllers/calculators-bike-insurance.ts
@@ -0,0 +1,7 @@
+/**
+ * calculators-bike-insurance controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::calculators-bike-insurance.calculators-bike-insurance');
---
+++ src/api/calculators-car-insurance/services/calculators-car-insurance.ts
@@ -0,0 +1,7 @@
+/**
+ * calculators-car-insurance service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::calculators-car-insurance.calculators-car-insurance');
